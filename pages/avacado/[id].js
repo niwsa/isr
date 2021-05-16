4675daf39fb38dae0dc6a8b42955d1b5ca59fbe7
Aswin V <vaswin91@gmail.com>
2021-05-16 23:30:07 +0530

    Changes to test isr behavior

---
+++ pages/avacado/[id].js
@@ -0,0 +1,35 @@
+import Layout from "components/layout";
+import styles from "styles/Card.module.css";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+export default function Avacado({ ts, id }) {
+  const { isFallback } = useRouter();
+  return (
+    <Layout ts={ts} id={id} isFallback={isFallback}>
+      <div className={styles.card}>
+        <Image src="/avacado.jpeg" width="400" height="350" />
+      </div>
+    </Layout>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const ts = new Date().toLocaleString();
+  const { id } = params;
+  console.log(`revalidating 🥑 ${id}`);
+  return {
+    props: {
+      ts,
+      id: `${id} 🐶🦁🐱`,
+    },
+    revalidate: 1,
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { id: "1" } }],
+    fallback: "blocking",
+  };
+}
---
+++ pages/orange/[id].js
@@ -0,0 +1,35 @@
+import Layout from "components/layout";
+import styles from "styles/Card.module.css";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+export default function Orange({ ts, id }) {
+  const { isFallback } = useRouter();
+  return (
+    <Layout ts={ts} id={id} isFallback={isFallback}>
+      <div className={styles.card}>
+        <Image src="/orange.jpeg" width="400" height="350" />
+      </div>
+    </Layout>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const ts = new Date().toLocaleString();
+  const { id } = params;
+  console.log(`revalidating 🍊 ${id}`);
+  return {
+    props: {
+      ts,
+      id: `${id} 🐶🦁🐱`,
+    },
+    revalidate: 1,
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { id: "1" } }],
+    fallback: "blocking",
+  };
+}
